755c91328ba4800d5147e6601d6cf4114deb3d76
Hentioe <meow.i5.br@gmail.com>
2024-05-21 20:31:25 +0800

    Add sha256 script

---
+++ sha256.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+RUSTUP_VERSION=1.27.1
+
+function get_sha256() {
+    local arch=$1
+    local url="https://static.rust-lang.org/rustup/archive/${RUSTUP_VERSION}/${arch}/rustup-init"
+    echo "rustup/${RUSTUP_VERSION}/${arch} $(curl -sSL ${url} | sha256sum | awk '{print $1}')"
+}
+
+get_sha256 "aarch64-unknown-linux-gnu"
+get_sha256 "x86_64-unknown-linux-gnu"
